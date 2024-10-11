636086e4b8fcc44adb804274572e6ba7dd25a6b1
Phillip Jhon Lumod <philliplumod@gmail.com>
2024-10-11 21:01:26 +0800

    Refactor utility functions and add UI components
    - Refactor the `cn` utility function in `lib/utils.ts` to use the `twMerge` function from the `tailwind-merge` package.
    - Add new UI components: `AspectRatio`, `Skeleton`, `Collapsible`, `Label`, `Textarea`, `Separator`, `Toaster`, `Progress`, `Input`, and `Checkbox`.
    - Update the `Logo` component to fix a typo and improve the code formatting.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,15 +1,15 @@
 import Logo from '@/components/Logo'
 import React from 'react'
 
-function layout({children} : {children: React.ReactNode}) {
+function Layout({children} : {children: React.ReactNode}) {
   return (
-    <div className='relative flex h-screen w-full flex-col items-center justify-center'>
+    <div className='relative flex min-h-screen w-full flex-col items-center justify-center p-4 sm:p-8 md:p-12 lg:p-16'>
         <Logo/>
-        <div className='mt-12'>
+        <div className='mt-8 sm:mt-12'>
             {children}
         </div>
     </div>
   )
 }
 
-export default layout
+export default Layout
