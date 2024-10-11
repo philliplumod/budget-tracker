e42bf81e65713d77bb049b4c4cd8ca89b9020bdc
Phillip Jhon Lumod <philliplumod@gmail.com>
2024-10-11 21:20:07 +0800

    Refactor utility functions and add UI components

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,15 +1,13 @@
-import Logo from '@/components/Logo'
-import React from 'react'
+import Logo from '@/components/Logo';
+import React from 'react';
 
-function Layout({children} : {children: React.ReactNode}) {
+function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='relative flex min-h-screen w-full flex-col items-center justify-center p-4 sm:p-8 md:p-12 lg:p-16'>
-        <Logo/>
-        <div className='mt-8 sm:mt-12'>
-            {children}
-        </div>
+    <div className="relative flex min-h-screen w-full flex-col items-center justify-center p-4 sm:p-8 md:p-12 lg:p-16">
+      <Logo />
+      <div className="mt-8 sm:mt-12">{children}</div>
     </div>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
